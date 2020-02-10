9ce514f633532d446d17c13007e346b53bf7a86a
wildan <48831840+wildanumarzent@users.noreply.github.com>
2020-02-10 11:28:25 +0700

    TodoForm active to alert

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const handleFormSubmit = e => {
+    e.preventDefault();
 
-const TodoForm = () => {
+    if (!value) {
+      alert("no blank todo");
+      return;
+    }
+    addTodo(value);
+    alert(value);
+    setValue("");
+  };
+  console.log(value);
   return (
     <section className="add">
-      <form className="add-form" action="">
-        <input type="text" className="add-input" />
+      <form className="add-form" action="" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
         <button className="add-btn">Add</button>
       </form>
     </section>
   );
+};
+TodoForm.propTypes = {
+  addtodo: PropTypes.func.isRequired
 };
 export default TodoForm;
